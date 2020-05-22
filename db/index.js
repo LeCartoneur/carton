d92c531262429244525637ae34f6ff6c63630341
thomas-god <thomas.godlewski@icloud.com>
2020-05-22 08:57:55 +0200

    Ajout CORS à l'API

--- db/index.js
+++ db/index.js
@@ -1,9 +1,11 @@
 const connectDb = require("./src/connection");
 const express = require("express");
+const cors = require("cors");
 const cartons_routes = require("./src/routes/cartons");
 const port = process.env.PORT || 8000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/cartons", cartons_routes);
 
