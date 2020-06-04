0336c5064a1ad1bc558fc00ec79234659715ee0c
thomas-god <thomas.godlewski@icloud.com>
2020-06-04 21:48:33 +0200

    Fix Mongoose `useFindAndModify` deprecation warning
    Based on https://mongoosejs.com/docs/deprecations.html#findandmodify.

--- db/src/connection.js
+++ db/src/connection.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Carton = require("./Carton.model");
 const connection = "mongodb://mongo:27017/cartons";
 
+mongoose.set("useFindAndModify", false);
+
 const connectDb = () => {
   return mongoose.connect(connection, {
     useUnifiedTopology: true,
