49d050ae2a13e63b5dfeb2d5dac67f812f006645
thomas-god <thomas.godlewski@icloud.com>
2020-06-07 10:59:59 +0200

    Add actual carton content from the DB in components
    Suite séance de travail du 06/06.

--- front/src/components/Carton.vue
+++ front/src/components/Carton.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="carton">
-    <h1>{{ nom }}</h1>
+    <h1>{{ carton.nom }}</h1>
     <div class="volets-container">
-      <volet v-for="volet in volets" :key="volet" :type="volet"></volet>
+      <volet
+        v-for="volet in volets"
+        :key="volet"
+        :type="volet"
+        :data="carton.versions[carton_version][volet]"
+      ></volet>
     </div>
   </div>
 </template>
@@ -16,7 +21,35 @@ export default {
     return {
       nom: 'Ordinateur',
       volets: ['quoi', 'comment', 'fonction'],
+      carton: {},
+      carton_version: 0,
     };
+  },
+  methods: {
+    getCarton(id, version) {
+      fetch('http://localhost:8000/cartons/get', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+          sous_carton: true,
+        }),
+      })
+        .then((response) => {
+          response.json().then((carton) => {
+            this.carton = carton;
+            this.carton_version = version;
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    this.getCarton('5edba60ab4056b0019b1a988', 0);
   },
 };
 </script>
@@ -36,5 +69,6 @@ export default {
   flex-direction: row;
   flex-wrap: nowrap;
   align-items: stretch;
+  justify-content: space-between;
 }
 </style>
--- front/src/components/Volet.vue
+++ front/src/components/Volet.vue
@@ -1,11 +1,7 @@
 <template>
   <div v-if="is_open_volet" :style="volet">
     <h2 @click="toggleVolet" :style="style">{{ config.titre }} -</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad molestiae veniam fugiat provident
-      quo doloribus repudiandae non? Quia, et dolores. Deserunt ratione vero excepturi!
-      Reprehenderit quae excepturi mollitia corporis earum!
-    </p>
+    <p v-html="texte_format"></p>
     <button @click="toggleSousCarton">{{ is_open_sous_carton ? 'Masquer' : 'Afficher' }}</button>
     <sous-carton v-if="is_open_sous_carton" />
   </div>
@@ -15,7 +11,7 @@
 </template>
 
 <script>
-import SousCarton from './SousCarton.vue';
+import SousCarton from './SousCarton.vue'
 
 const configs = {
   fonction: {
@@ -30,42 +26,66 @@ const configs = {
     titre: "Komment ça l'fait",
     color: 'blue',
   },
-};
+}
 
 export default {
   components: {
     SousCarton,
   },
-  props: ['type'],
+  // TODO: use correct Vue.js props definition/syntax
+  props: ['type', 'data'],
   data() {
     return {
       is_open_volet: true,
       is_open_sous_carton: false,
       current_id_sous_carton: '',
-    };
+      texte_format: '',
+    }
   },
   computed: {
     config() {
-      return configs[this.type];
+      return configs[this.type]
     },
     style() {
-      return `background-color: ${this.config.color}`;
+      return `background-color: ${this.config.color}`
     },
     volet() {
-      return `margin: 1rem;
-  padding: 1rem;
-  border: 1px solid ${this.config.color};`;
+      return `
+        margin: 1rem;
+        padding: 1rem;
+        border: 1px solid ${this.config.color};
+        flex-grow: 1;
+        flex-shrink: 1;
+      `
+    },
+  },
+  watch: {
+    data() {
+      this.formatText()
     },
   },
   methods: {
     toggleSousCarton() {
-      this.is_open_sous_carton = !this.is_open_sous_carton;
+      this.is_open_sous_carton = !this.is_open_sous_carton
     },
     toggleVolet() {
-      this.is_open_volet = !this.is_open_volet;
+      this.is_open_volet = !this.is_open_volet
+    },
+    formatText() {
+      const regex = /{([^}]+)}/g
+      this.texte_format = this.data.texte.replace(regex, (id) => {
+        return `
+          <span>
+          ${this.data.sous_cartons.find((carton) => carton._id === id.slice(1, -1)).nom}
+          </span>
+        `
+      })
     },
   },
-};
+  mounted() {
+    this.formatText()
+  },
+}
 </script>
 
 <style>
