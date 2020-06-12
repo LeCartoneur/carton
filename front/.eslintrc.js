68080f33bc12e8eb0bb88642e334cfa621c15a96
thomas-god <thomas.godlewski@icloud.com>
2020-06-12 20:25:34 +0200

    Update eslint config file
    Duplicate between plugins

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/prettier',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
@@ -16,7 +11,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-}
+};
