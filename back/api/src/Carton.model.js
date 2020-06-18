29922c9dc149f505bcb950a85a0d5c70da81fd66
thomas-god <thomas.godlewski@icloud.com>
2020-06-18 08:35:56 +0200

    Update the Carton Mongoose schema to have default values
    Not having all fields populated (like missing versions[0].fonction.texte)
    could break the frontend that needs to parse these fields. Now they
    get default values (empty strings and arrays) when creating a new
    carton document.
    The only remaining fields not having a default value is the versions
    array: it is still possible to create a carton without any version. Maybe
    this should be fixed in the future.

--- back/api/src/Carton.model.js
+++ back/api/src/Carton.model.js
@@ -1,58 +1,53 @@
 const mongoose = require("mongoose");
 
+const model_sous_carton = {
+  carton_id: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  version_id: { type: Number, default: 0 },
+};
+
+const model_category = {
+  texte: {
+    type: String,
+    default: () => "",
+  },
+  sous_cartons: {
+    type: [model_sous_carton],
+    default: () => [],
+  },
+};
+
 const cartonSchema = new mongoose.Schema({
-  user: String,
+  user: {
+    type: String,
+    default: "default",
+  },
   parent: {
     type: mongoose.Types.ObjectId,
+    default: null,
   },
-  nom: String,
+  nom: {
+    type: String,
+    required: true,
+  },
   private: { type: Boolean, default: false },
   versions: [
     {
-      nom: String,
-      quoi: {
-        texte: String,
-        sous_cartons: [
-          {
-            carton_id: mongoose.Types.ObjectId,
-            version_id: { type: Number, default: 0 },
-          },
-        ],
+      nom: {
+        type: String,
+        default: "",
       },
-      fonction: {
-        texte: String,
-        sous_cartons: [
-          {
-            carton_id: mongoose.Types.ObjectId,
-            version_id: { type: Number, default: 0 },
-          },
-        ],
-      },
-      comment: {
-        texte: String,
-        sous_cartons: [
-          {
-            carton_id: mongoose.Types.ObjectId,
-            version_id: { type: Number, default: 0 },
-          },
-        ],
-      },
-      exemples: {
-        texte: String,
-        sous_cartons: [
-          {
-            carton_id: mongoose.Types.ObjectId,
-            version_id: { type: Number, default: 0 },
-          },
-        ],
-      },
+      quoi: model_category,
+      fonction: model_category,
+      comment: model_category,
+      exemples: model_category,
       plus_loin: {
-        sous_cartons: [
-          {
-            carton_id: mongoose.Types.ObjectId,
-            version_id: { type: Number, default: 0 },
-          },
-        ],
+        sous_cartons: {
+          type: [model_sous_carton],
+          default: () => [],
+        },
       },
     },
   ],
