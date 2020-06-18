LeCartoneur/carton

commit ae1a4f56e5aac4ec4b70cd6d1882fec487e86886

Update volets to be a computed, and display all in editor
More vue friendly to have a computed property.
In editor mode we want to see all categories to update them.

front/src/components/Carton.vue
@@ -52,11 +52,24 @@ export default {
       cartons_originels: [],
       carton: {},
       carton_version: 0,
-      volets: [],
       mode_actif: 0, //0: Visionneuse ||1: Editeuse ||2: Commenteuse
     }
   },
   computed: {
+    volets() {
+      let volets = []
+      for (let cat of ['quoi', 'fonction', 'comment']) {
+        let item = this.carton.versions[this.carton_version][cat]
+        if (
+          item.sous_cartons.length > 0 ||
+          item.texte !== '' ||
+          this.mode_actif === 1 // On affiche tout en mode éditeur
+        ) {
+          volets.push({ cat: cat, reduced: false })
+        }
+      }
+      return volets
+    },
     n_volets() {
       return this.volets.length > 0 ? this.volets.filter((vol) => vol.reduced).length : 0
     },
@@ -79,7 +92,6 @@ export default {
         response.json().then((carton) => {
           this.carton = carton
           this.carton_version = version
-          this.updateVoletsList()
         })
       })
     },
@@ -91,19 +103,6 @@ export default {
           this.cartons_originels = cartons_originels
         })
       })
-    },
-    updateVoletsList() {
-      let volets = []
-      for (let cat of ['quoi', 'fonction', 'comment']) {
-        let item = this.carton.versions[this.carton_version][cat]
-        if (
-          (item.sous_cartons ? item.sous_cartons.length > 0 : false) ||
-          (item.texte ? item.texte !== '' : false)
-        ) {
-          volets.push({ cat: cat, reduced: false })
-        }
-      }
-      this.volets = volets
     },
     toggleVolet(volet) {
       volet.reduced = !volet.reduced

commit 526a3bec3e7a9b1a01ee8e8059d33bece11c0075

Fix issue in volet reduced not being updated
Fix regression introduced by ae1a4f56e5aac4ec4b70cd6d1882fec487e86886.

Moving `volets` to be a computed property was a mistake as its items must
 be updatable by the `toggle-component` event.

front/src/components/Carton.vue
@@ -41,25 +41,10 @@ export default {
       carton_version: 0,
       carton_ready: false,
       mode_actif: 0, //0: Visionneuse ||1: Editeuse ||2: Commenteuse
+      volets: [],
     }
   },
   computed: {
-    volets() {
-      let volets = []
-      if (this.carton_ready) {
-        for (let cat of ['quoi', 'fonction', 'comment']) {
-          let item = this.carton.versions[this.carton_version][cat]
-          if (
-            item.sous_cartons.length > 0 ||
-            item.texte !== '' ||
-            this.mode_actif === 1 // On affiche tout en mode éditeur
-          ) {
-            volets.push({ cat: cat, reduced: false })
-          }
-        }
-      }
-      return volets
-    },
     n_volets() {
       return this.volets.length > 0 ? this.volets.filter((vol) => vol.reduced).length : 0
     },
@@ -85,7 +70,22 @@ export default {
         .then((carton) => {
           this.carton = carton
           this.carton_ready = true
+          this.loadVolets()
         })
+    },
+    loadVolets() {
+      let volets = []
+      for (let cat of ['quoi', 'fonction', 'comment']) {
+        let item = this.carton.versions[this.carton_version][cat]
+        if (
+          item.sous_cartons.length > 0 ||
+          item.texte !== '' ||
+          this.mode_actif === 1 // On affiche tout en mode éditeur
+        ) {
+          volets.push({ cat: cat, reduced: false })
+        }
+      }
+      this.volets = volets
     },
     toggleVolet(volet) {
       volet.reduced = !volet.reduced