067198cafa3fc7402d66d043b227ecdba471ca8a
thomas-god <thomas.godlewski@icloud.com>
2020-06-25 13:08:43 +0200

    Move Mongoose models into a dedicated folder

--- back/api/src/connection.js
+++ back/api/src/connection.js
@@ -6,13 +6,16 @@ const connDb = mongoose.createConnection(process.env.MONGO_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-const Carton = connDb.model("Carton", require("./Carton.model"));
+const Carton = connDb.model("Carton", require("./models/Carton.model"));
 
 const connDbUsers = mongoose.createConnection(process.env.MONGO_URL_USERS, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-const User = connDbUsers.model("User", require("./User.model.js"));
-const AlphaKey = connDbUsers.model("AlphaKey", require("./AlphaKey.model.js"));
+const User = connDbUsers.model("User", require("./models/User.model.js"));
+const AlphaKey = connDbUsers.model(
+  "AlphaKey",
+  require("./models/AlphaKey.model.js")
+);
 
 module.exports = { Carton, User, AlphaKey, connDb, connDbUsers };
