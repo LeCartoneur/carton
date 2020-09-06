6980c1d1238528d020c0833e7a80dc50d555f33b
thomas-god <thomas.godlewski@icloud.com>
2020-09-06 11:06:19 +0200

    Update Accueil page

--- front/src/views/Accueil.vue
+++ front/src/views/Accueil.vue
@@ -7,5 +7,35 @@
       décomposées en sous-notions.
     </p>
 
+    <button @click="$router.push({ path: `/carton` })">Parcourir les cartons</button>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  margin: 2rem auto 1rem;
+  text-align: center;
+}
+
+p {
+  margin: auto;
+  width: 600px;
+  font-size: 1.2rem;
+}
+
+button {
+  padding: 10px;
+  margin: 1rem auto;
+  display: block;
+  border-radius: 3px;
+  border: 1px solid black;
+  font-size: 1.1rem;
+  background-color: blanchedalmond;
+}
+
+button:hover {
+  padding: 9px;
+  border: 2px solid black;
+  background-color: rgb(251, 219, 171);
+}
+</style>
