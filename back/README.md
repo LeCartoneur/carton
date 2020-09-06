f6d9df9521a0e13260272f578a44a0ab77df2f9b
thomas-god <thomas.godlewski@icloud.com>
2020-09-06 07:30:57 +0200

    Update project structure

--- back/README.md
+++
@@ -1,3 +0,0 @@
-## Lancer l'API avec `docker-compose`
-
-`sudo docker-compose -f ./api/docker-compose.yml -f docker-compose.yml up --build -d`
--- back/ssl_renew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
-
-cd /home/thomas/carton/back/
-$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
-$DOCKER system prune -af
